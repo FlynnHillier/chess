45cdd7171d898c3f4ff66f175ae77d73e306b568
FlynnHillier <flynnhillier@gmail.com>
2022-06-25 11:19:07 +0100

    implemented session handling, with mongo storage.

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,10 @@ import path from "path"
 const config = {
     express:{
         port:5000,
+        session:{
+            secret:process.env.secret || "Lima Mike Alpha Oscar",
+            mongo_store_uri:process.env.MONGO_URI
+        }
     },
     logs:{
         outDir:path.join(__dirname,"logs")
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,32 @@
-import * as express from "express"
-import ErrorLogger from "./util/logging/ErrorLog"
+import config from "./config"
 
+import * as express from "express"
 import apiRouter from "./api/api-router"
 
+import session, { MemoryStore, Store } from "express-session"
+import MongoSessionStore from "connect-mongo"
+
+import ErrorLogger from "./util/logging/ErrorLog"
 
 const router = express.Router()
+
+
+router.use(session({
+    secret:config.express.session.secret,
+    saveUninitialized:true,
+    resave:true,
+    cookie:{
+        sameSite:false,
+        secure:process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 14, //2 weeks
+    },
+    store:MongoSessionStore.create({
+        mongoUrl:config.express.session.mongo_store_uri,
+        ttl:60 * 60 * 24 * 14, //2 weeks
+        autoRemove:"interval",
+        autoRemoveInterval:10,
+    }) as unknown as Store
+}))
 
 router.use("a",apiRouter)
 
