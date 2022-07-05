a2738e42dcdc7ad79ad5e906b5d3b018c34eb9ab
FlynnHillier <flynnhillier@gmail.com>
2022-07-05 17:31:58 +0100

    .env file path fix

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,7 +1,7 @@
 import path from "path"
 
 import dotenv from "dotenv"
-dotenv.config({path:path.resolve(__dirname,"..","..",".env")})
+dotenv.config({path:path.resolve(__dirname,"..","..","..",".env")})
 
 import config from "./config" 
 import express from "express"
