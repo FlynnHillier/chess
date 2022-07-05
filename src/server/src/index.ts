ee3c0214d3a4c20f0b5a9925aca0a659f54cc2da
flynnHillier <flynnhillier@gmail.com>
2022-07-05 11:12:25 +0100

    folder structure re-configuration. Fixed compilation module location errors.

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,12 +1,11 @@
 import path from "path"
 
 import dotenv from "dotenv"
-dotenv.config({path:path.resolve(__dirname,"..",".env")})
+dotenv.config({path:path.resolve(__dirname,"..","..",".env")})
 
 import config from "./config" 
 import express from "express"
 import router from "./router"
-
 import mongoose from "mongoose"
 
 const app = express()
