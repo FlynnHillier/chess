9c98711cc5ae0a7b5e8954cc0de1e5f32a33c8b5
FlynnHillier <flynnhillier@gmail.com>
2022-08-05 17:49:04 +0100

    initial piece movement logic for react implementation

--- src/client/src/components/ChessTile.tsx
+++ src/client/src/components/ChessTile.tsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import {Piece} from "chess-online-common"
+import React, { ReactElement,useEffect,useState } from 'react'
+import {Piece,Perspective, Species,Coordinate} from "chess-online-common"
+import { ChessPiece } from './ChessPiece'
 
 interface Props {
     occupant:null | Piece
-    isDarkTile:boolean
-    style?:React.CSSProperties
+    position:Coordinate
+    isDarkTile:boolean,
+    setHoveredTile:(arg:Coordinate | null)=>void,
+    selectedPieceCoordinate:Coordinate | null
 }
 
 
-const ChessTile = (props:Props) => {
+const ChessTile = ({occupant,position,isDarkTile,setHoveredTile,selectedPieceCoordinate}:Props) => {
+  let [isSelected,setIsSelected] = useState<boolean>(JSON.stringify(selectedPieceCoordinate) === JSON.stringify(position))
+  let [pieceImageOffset,setPieceImageOffset] = useState<{x:number,y:number}>({x:0,y:0})
+
+  function onMouseEnter(){
+    setHoveredTile(position)
+  }
+  
+  function onMouseLeave(){
+    setHoveredTile(null)
+  }
+
+  function onMouseMove(){
+    if(isSelected){
+      
+    }
+  }
+
+  useEffect(()=>{
+    setIsSelected(JSON.stringify(selectedPieceCoordinate) === JSON.stringify(position))
+  },[selectedPieceCoordinate])
+
   return (
-    <div  
-      className={`p-0 chess-tile chess-tile-${props.isDarkTile ? "dark" : "light"} ${props.occupant ? "occupied" : ""}`}
-      style={props.style}
+    <div
+      className={`chess-tile chess-tile-${isDarkTile ? "dark" : "light"} ${occupant ? "occupied" : ""}`}
+      onMouseEnter={onMouseEnter}  
+      onMouseLeave={onMouseLeave}
+      onMouseMove={onMouseMove}
+      style={{
+        "gridColumnStart":position[0] +1,
+        "gridRowStart":position[1] + 1,
+      }}
     >
-      {
-        props.occupant ? <img alt={`${props.occupant.perspective} ${props.occupant.species}`} src={require(`./../assets/chessPieceImage/${props.occupant.perspective}/${props.occupant.species}.png`)}/> : <></>
-      }
+      {occupant ? <ChessPiece imageOffset={pieceImageOffset} species={occupant.species} perspective={occupant.perspective}/> : <></>}
     </div>
   )
 }
--- src/client/src/components/Chessboard.tsx
+++ src/client/src/components/Chessboard.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 import "./../styles/chessboard.css"
 
 import { Container } from 'react-bootstrap'
-import {Board} from "chess-online-common"
+import {Board,Piece,Coordinate} from "chess-online-common"
 
 import ChessTile from "./ChessTile"
 
@@ -12,27 +13,73 @@ interface Props {
 
 
 const Chessboard = (props:Props) => {
-  return (
-    <Container fluid className="d-flex justify-content-center p-0">
-        <div className="chessboard">
-            {
-                props.game.tileMap.map((tile,index)=>{
-                    return (
-                        <ChessTile
-                            key={`key_chess-tile-${index}`}
-                            occupant={tile.occupant}
-                            isDarkTile={(((index % props.game.rowLength) + 1) % 2 === 0 && ((Math.floor(index / props.game.rowLength) + 1) % 2 !== 0)) || (((index % props.game.rowLength) + 1) % 2 !== 0 && ((Math.floor(index / props.game.rowLength) + 1) % 2 === 0))}
-                            style={{
-                                "gridColumnStart":(index % props.game.rowLength) + 1,
-                                "gridRowStart":Math.floor(index / props.game.rowLength) + 1,
-                            }}
-                        />
-                    )
-                })
+    let [hoveredTile,setHoveredTile] = useState<null | [number,number]>(null)
+    let [selectedPiece,setSelectedPiece] = useState<null | Piece>(null)
+    let [selectedPieceCoordinate,setSelectedPieceCoordinate] = useState<null | Coordinate>(null)
+
+
+    useEffect(()=>{
+        console.log(hoveredTile)
+    },[hoveredTile])
+
+    useEffect(()=>{
+        if(selectedPiece !== null){
+            setSelectedPieceCoordinate(selectedPiece?.location)
+        } else{
+            setSelectedPieceCoordinate(null)
+        }
+        console.log(selectedPiece)
+    },[selectedPiece])
+
+    function onMouseDown(){
+        if(hoveredTile !== null){
+            const hoveredTileOccupant = props.game.getTile(hoveredTile).occupant    
+            setSelectedPiece(hoveredTileOccupant)
+        }
+    }
+
+    function onMouseUp(){
+        if(selectedPiece){
+            if(hoveredTile !== null && JSON.stringify(hoveredTile) !== JSON.stringify(selectedPieceCoordinate)){
+                if(selectedPiece.movableTo.length !== 0 && selectedPiece.movableTo.some(a=> hoveredTile?.every((v,i)=> v === a[i]))){
+                    console.log("moving to")
+                    selectedPiece.move(hoveredTile)
+                }
             }
-        </div>
-    </Container>
-  )
+            setSelectedPiece(null)
+        }
+    }
+
+    return (
+        <Container fluid className="d-flex justify-content-center p-0">
+            <div 
+                className="chessboard"
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+            >
+                {
+                    props.game.tileMap.map((tile,index)=>{
+                        const occupant = tile.occupant
+                        const location = {
+                            x:(index % props.game.rowLength),
+                            y:Math.floor(index / props.game.rowLength),
+                        }
+                        return(
+                            <ChessTile
+                                        key={`key_chess-tile-${index}`}
+                                        selectedPieceCoordinate={selectedPieceCoordinate}
+                                        setHoveredTile={setHoveredTile}
+                                        occupant={occupant}
+                                        isDarkTile={(((index % props.game.rowLength) + 1) % 2 === 0 && ((Math.floor(index / props.game.rowLength) + 1) % 2 !== 0)) || (((index % props.game.rowLength) + 1) % 2 !== 0 && ((Math.floor(index / props.game.rowLength) + 1) % 2 === 0))}
+                                        position={[location.x,location.y]}
+                            />
+                        )
+                    })
+                }
+
+            </div>
+        </Container>
+    )
 }
 
 export default Chessboard
